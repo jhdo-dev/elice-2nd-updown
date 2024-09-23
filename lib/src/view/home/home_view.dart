4a6f36ba62eeb968dadb0035fadbde45dfa420c1
zihoon9 <zihoon9@gmail.com>
2024-09-23 18:42:07 +0900

    [fix] HomeView 분리

--- lib/src/view/home/home_view.dart
+++ lib/src/view/home/home_view.dart
@@ -23,23 +23,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Column(
-        children: [
-          // 인기 방 스와이프 가능한 섹션
-          SizedBox(
-            height: 150, // 원하는 높이로 설정
-            child: PageView(
-              children: [
-                _buildPopularRoomCard('인기 방 1'),
-                _buildPopularRoomCard('인기 방 2'),
-                _buildPopularRoomCard('인기 방 3'),
-              ],
-            ),
-          ),
-          // 방 리스트
-          const Expanded(child: RoomListPage()),
-        ],
-      ),
+      body: const HomeView(), // HomeView 호출
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: _selectedIndex,
@@ -54,6 +38,31 @@ class _HomePageState extends State<HomePage> {
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'MyPage'),
         ],
       ),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // 인기 방 스와이프 가능한 섹션
+        SizedBox(
+          height: 150, // 원하는 높이로 설정
+          child: PageView(
+            children: [
+              _buildPopularRoomCard('인기 방 1'),
+              _buildPopularRoomCard('인기 방 2'),
+              _buildPopularRoomCard('인기 방 3'),
+            ],
+          ),
+        ),
+        // 방 리스트
+        const Expanded(child: RoomListPage()),
+      ],
     );
   }
 
