3341f28ab99206329f5bba4676bff02c9cbc9965
[APP1] 김동혁 <hayo970323@gmail.com>
2024-09-23 11:51:05 +0000

    Revert "Merge branch 'feature/result' into 'main'"
    This reverts merge request !3

--- lib/util/route_names.dart
+++
@@ -1,7 +0,0 @@
-class RouteNames {
-  static const home = 'home';
-  static const auth = 'auth';
-  static const chat = 'chat';
-  static const result = 'result';
-  static const setting = 'setting';
-}
