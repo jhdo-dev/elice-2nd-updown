b06a647b80fb07a3e34f78de5c2d561b60764bb8
[APP1]이영인 <zeroinlee@gmail.com>
2024-09-25 16:01:57 +0900

    [feature] 비밀번호 재설정 페이지 추가 및 라우팅 업데이트

--- lib/util/router/route_names.dart
+++ lib/util/router/route_names.dart
@@ -1,6 +1,7 @@
 class RouteNames {
   static const signin = 'signin';
   static const signup = 'signup';
+  static const password = 'password';
   static const home = 'home';
   static const auth = 'auth';
   static const chat = 'chat';
