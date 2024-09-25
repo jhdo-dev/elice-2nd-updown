1b40a9e00b73d1ecdee4141b3544ef8ec0b8d975
[APP1]이영인 <zeroinlee@gmail.com>
2024-09-25 13:21:59 +0900

    [feature] 사용자 로그인 시 'Remember Me' 기능 추가 및 자동 로그인 구현

--- lib/src/view/auth/pages/welcome_page.dart
+++ lib/src/view/auth/pages/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -11,11 +12,14 @@ class WelcomePage extends StatefulWidget {
 
 class _WelcomePageState extends State<WelcomePage> {
   final _auth = FirebaseAuth.instance;
+  final user = FirebaseAuth.instance.currentUser;
 
   Future<void> _signOut() async {
     try {
       // Firebase Authentication을 사용하여 로그아웃을 구현하세요.
       await _auth.signOut();
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove('userId');
 
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Successfully signed out!')),
@@ -36,7 +40,7 @@ class _WelcomePageState extends State<WelcomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Welcome'),
+            Text('Welcome! ${user?.displayName}'),
             ElevatedButton(
               onPressed: _signOut,
               child: const Text('Sign out'),
