012733400d7cc9d6ab44e87e65f9944277292100
[APP1] 국한 <kook1488@naver.com>
2024-09-28 18:57:44 +0900

    알림코드 다듬기

--- lib/services/fcm/fcm_service.dart
+++ lib/services/fcm/fcm_service.dart
@@ -23,4 +23,17 @@ class FCMService {
     // TODO: 실제 서버 API를 호출하여 토큰 저장
     print("Sending token to server: $token");
   }
+
+  Future<void> requestPermissions() async {
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    print('User granted permission: ${settings.authorizationStatus}');
+  }
 }
