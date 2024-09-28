8f1cd776f8e2f6e444ed55e3f00073211154d7a6
[APP1] 국한 <kook1488@naver.com>
2024-09-28 20:21:16 +0900

    진척은되는데 왜 알림은 안오지?

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,28 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+// FCM 알림 전송 함수
+exports.sendNotification = functions.https.onCall((data, context) => {
+  const message = {
+    notification: {
+      title: data.title || '알림 제목',
+      body: data.body || '알림 내용'
+    },
+    topic: data.topic || 'all_users' // 또는 특정 토픽이나 토큰
+  };
+
+  return admin.messaging().send(message)
+    .then((response) => {
+      console.log('Successfully sent message:', response);
+      return {success: true};
+    })
+    .catch((error) => {
+      console.log('Error sending message:', error);
+      return {success: false};
+    });
+});
 
 // 첫 번째 HTTP 함수 - 텍스트 반환
 exports.helloFunction = functions.https.onRequest((req, res) => {
