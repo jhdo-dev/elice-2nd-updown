761ea128c5a1922f7a9d383fb63f2d57e57ca855
[APP1]이영인 <nyile4423@gmail.com>
2024-09-30 02:30:30 +0900

    [feature] 프로필 관련 리포지토리 및 프로바이더 추가; 비밀번호 재설정 기능 구현; 라우팅 경로 수정

--- lib/src/view/setting/setting_view.dart
+++ lib/src/view/setting/setting_view.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:up_down/src/view/setting/profile/profile_widget.dart';
 
-import '../auth/widgets/error_dialog.dart';
+import '../../../component/error_dialog.dart';
 import '../../../util/helper/firebase_helper.dart';
+import '../../../util/router/route_names.dart';
 import '../../model/custom_error.dart';
 import '../../provider/auth_repository_provider.dart';
+import 'profile/profile_provider.dart';
 
 class SettingView extends ConsumerStatefulWidget {
   const SettingView({super.key});
@@ -27,22 +31,62 @@ class _SettingViewState extends ConsumerState<SettingView> {
   @override
   Widget build(BuildContext context) {
     final uid = fbAuth.currentUser!.uid;
-    // final profileState = ref.watch(profileProvider(uid));
+    final profileState = ref.watch(profileProvider(uid));
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Welcome!'),
-            // Text('name: ${_user?.displayName}'),
-            // Text('email: ${_user?.email}'),
-            // Text('photo: ${_user?.photoURL}'),
-            ElevatedButton(
-              onPressed: _signOut,
-              child: const Text('Sign out'),
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.invalidate(profileProvider);
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      body: profileState.when(
+        skipLoadingOnRefresh: false,
+        data: (appUser) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const ProfileWidget(),
+                const Text('Welcome!'),
+                Text('name: ${appUser.name}'),
+                Text('email: ${appUser.email}'),
+                // Text('photo: ${appUser.photoURL}'),
+                ElevatedButton(
+                  onPressed: _signOut,
+                  child: const Text('Sign out'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // GoRouter.of(context).goNamed(RouteNames.changePassword);
+                  },
+                  child: const Text('Change Password'),
+                )
+              ],
             ),
-          ],
+          );
+        },
+        error: (e, _) {
+          final error = e as CustomError;
+
+          return Center(
+            child: Text(
+              'code: ${error.code}\nplugin: ${error.plugin}\nmessage: ${error.message}',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.red,
+                fontSize: 18,
+              ),
+            ),
+          );
+        },
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
         ),
       ),
     );
