f39134712e7e35ddf5d9a3fba4281e746f7d37b3
[APP1]이영인 <zeroinlee@gmail.com>
2024-09-30 21:35:24 +0900

    [refactor] 프로필 관련 파일을 설정 관련 파일로 리팩토링; 불필요한 프로필 관련 코드 삭제 및 경로 수정

--- lib/src/view/setting/setting_provider.g.dart
+++ lib/src/view/setting/setting_provider.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'profile_provider.dart';
+part of 'setting_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
