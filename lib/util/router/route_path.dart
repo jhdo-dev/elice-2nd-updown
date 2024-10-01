6b8712ed6d9cd77dfc95593757e8748c5fafe5bf
[APP1]이영인 <nyile4423@gmail.com>
2024-10-02 04:39:16 +0900

    [refactor] 불필요한 import 문 제거; rememberMe 기본값을 true로 변경

--- lib/util/router/route_path.dart
+++ lib/util/router/route_path.dart
@@ -2,10 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:up_down/component/page_not_found.dart';
 import 'package:up_down/component/scaffold_with_nav_bar.dart';
-import 'package:up_down/src/model/room.dart';
 import 'package:up_down/src/provider/auth_repository_provider.dart';
 import 'package:up_down/src/view/auth/auth_view.dart';
 import 'package:up_down/src/view/setting/setting_view.dart';
@@ -17,8 +15,6 @@ import 'package:up_down/src/view/result/result_view.dart';
 import 'package:up_down/src/view/splash/firebase_error_view.dart';
 import 'package:up_down/src/view/splash/splash_view.dart';
 import 'package:up_down/util/router/route_names.dart';
-
-import '../helper/firebase_helper.dart';
 
 part 'route_path.g.dart';
 
@@ -55,13 +51,7 @@ GoRouter route(RouteRef ref) {
 
         final splashing = state.matchedLocation == '/splash';
 
-        // SharedPreferences에서 rememberMe 값을 불러옴
-        final prefs = await SharedPreferences.getInstance();
-        final rememberMe = prefs.getBool('rememberMe') ?? false;
-        if (rememberMe) {
-          return (authenticating || splashing) ? '/home' : null;
-        }
-        return null;
+        return (authenticating || splashing) ? '/home' : null;
       },
       routes: [
         GoRoute(
