491b41a11f88099441795e6108f64eb133620fc1
[APP1]이영인 <nyile4423@gmail.com>
2024-10-02 03:33:15 +0900

    [feat] 추가: 계정 삭제 기능 및 관련 UI 구현; SharedPreferences를 통한 테마 저장 기능 추가; 구글 로그인 오류 해결;

---
+++ lib/src/view/setting/delete_account/delete_account_dialog.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import '../../../../component/error_dialog.dart';
+import '../../../model/custom_error.dart';
+import 'delete_account_provider.dart';
+
+class DeleteAccountDialog extends ConsumerStatefulWidget {
+  const DeleteAccountDialog({super.key});
+
+  @override
+  _DeleteAccountDialogState createState() => _DeleteAccountDialogState();
+}
+
+class _DeleteAccountDialogState extends ConsumerState<DeleteAccountDialog> {
+  bool submitting = false;
+
+  Future<void> _deleteAccount() async {
+    await ref.read(deleteAccountProvider.notifier).deleteAccount();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ref.listen<AsyncValue<void>>(
+      deleteAccountProvider,
+      (previous, next) {
+        next.whenOrNull(
+          error: (e, st) {
+            errorDialog(context, e as CustomError);
+          },
+          data: (_) {
+            GoRouter.of(context).pop();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Account deleted successfully'),
+              ),
+            );
+          },
+        );
+      },
+    );
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: AlertDialog(
+        title: const Text('Delete Account'),
+        content: const Text(
+            'Are you sure you want to delete your account?\nThis action cannot be undone.'),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              GoRouter.of(context).pop();
+            },
+            child: const Text('Cancle'),
+          ),
+          ElevatedButton(
+            onPressed: () => _deleteAccount(),
+            child: const Text('Delete'),
+          ),
+        ],
+      ),
+    );
+  }
+}
