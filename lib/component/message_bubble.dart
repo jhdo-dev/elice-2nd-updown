b65ea8227d7906f854d1eb576f9a830532cb2908
[APP1]이영인 <zeroinlee@gmail.com>
2024-10-02 23:45:02 +0900

    [feature] MessageBubble 제작

--- lib/component/message_bubble.dart
+++ lib/component/message_bubble.dart
@@ -36,37 +36,86 @@ class MessageBubble extends ConsumerWidget {
     return Column(
       crossAxisAlignment:
           isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: isMe
-              ? const EdgeInsets.fromLTRB(100, 5, 0, 5)
-              : const EdgeInsets.fromLTRB(0, 5, 100, 5),
-          child: Container(
-            decoration: BoxDecoration(
-              // 연속된 말풍선 색깔 설정
-              color: isMe
-                  ? Theme.of(context).colorScheme.primary
-                  : Theme.of(context).colorScheme.secondary,
-              borderRadius: BorderRadius.only(
-                bottomRight: const Radius.circular(15),
-                bottomLeft: const Radius.circular(15),
-                topRight:
-                    isMe ? const Radius.circular(0) : const Radius.circular(15),
-                topLeft:
-                    isMe ? const Radius.circular(15) : const Radius.circular(0),
+      children: !message.isMyTurn
+          ? [
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(message.name),
+                    const SizedBox(width: 12),
+                    Text(
+                      _formatDateTime(message.sentAt.toDate()).toString(),
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.white.withOpacity(0.5),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-            margin: isMe
-                ? const EdgeInsets.only(right: 14)
-                : const EdgeInsets.only(left: 14),
-            child: message.message.startsWith('http') // 메시지가 URL이면 이미지로 렌더링
-                //이미지 크기 세팅
-                ? Image.network(message.message)
-                : Text(message.message), // 텍스트 메시지,
-          ),
-        ),
-      ],
+              Padding(
+                padding: isMe
+                    ? const EdgeInsets.fromLTRB(100, 5, 0, 5)
+                    : const EdgeInsets.fromLTRB(0, 5, 100, 5),
+                child: Container(
+                  decoration: BoxDecoration(
+                    // 연속된 말풍선 색깔 설정
+                    color: isMe
+                        ? Theme.of(context).colorScheme.primary
+                        : Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.only(
+                      bottomRight: const Radius.circular(15),
+                      bottomLeft: const Radius.circular(15),
+                      topRight: isMe
+                          ? const Radius.circular(0)
+                          : const Radius.circular(15),
+                      topLeft: isMe
+                          ? const Radius.circular(15)
+                          : const Radius.circular(0),
+                    ),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  margin: isMe
+                      ? const EdgeInsets.only(right: 14)
+                      : const EdgeInsets.only(left: 14),
+                  child:
+                      message.message.startsWith('http') // 메시지가 URL이면 이미지로 렌더링
+                          //이미지 크기 세팅
+                          ? Image.network(message.message)
+                          : Text(message.message), // 텍스트 메시지,
+                ),
+              ),
+            ]
+          : [
+              Padding(
+                padding: isMe
+                    ? const EdgeInsets.fromLTRB(100, 5, 0, 5)
+                    : const EdgeInsets.fromLTRB(0, 5, 100, 5),
+                child: Container(
+                  decoration: BoxDecoration(
+                    // 연속된 말풍선 색깔 설정
+                    color: isMe
+                        ? Theme.of(context).colorScheme.primary
+                        : Theme.of(context).colorScheme.secondary,
+                    borderRadius: const BorderRadius.all(Radius.circular(15)),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  margin: isMe
+                      ? const EdgeInsets.only(right: 14)
+                      : const EdgeInsets.only(left: 14),
+                  child:
+                      message.message.startsWith('http') // 메시지가 URL이면 이미지로 렌더링
+                          //이미지 크기 세팅
+                          ? Image.network(message.message)
+                          : Text(message.message), // 텍스트 메시지,
+                ),
+              ),
+            ],
     );
   }
 }
