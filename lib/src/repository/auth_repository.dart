e44fa7f0b5bf70aa9c337c879b89f03ae60ad029
[APP1]이영인 <zeroinlee@gmail.com>
2024-10-03 02:29:46 +0900

    [refactor] 불필요한 import 문 제거

--- lib/src/repository/auth_repository.dart
+++ lib/src/repository/auth_repository.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart' as kUser;
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:up_down/util/helper/firebase_helper.dart';
